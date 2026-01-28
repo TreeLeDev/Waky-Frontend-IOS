11a22788ff5596c7aabc7be49e8bd5e8a3d12158
Tri Le <leqtri32@gmail.com>
2026-01-28 01:18:44 -0500

    Initial Commit

---
+++ Waky/WakyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakyApp.swift
+//  Waky
+//
+//  Created by Tri Le on 2026-01-28.
+//
+
+import SwiftUI
+
+@main
+struct WakyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
